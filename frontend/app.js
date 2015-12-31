f545b5f92c0f9dd2a83edfb19f564e6a34656e93
Ivan Kleshnin <ivan@paqmind.com>
2015-12-31 18:37:40 +0200

    Add UI to change hyperscript syntax and tab size
    Closes #5
    Closes #6

--- frontend/app.js
+++ frontend/app.js
@@ -15,9 +15,15 @@ import "./vendors/fontello/animation.css";
 import "./vendors/fontello/fontello.css";
 import "./styles/app.less";
 
-let convert2 = htmlToHs2({});
+let currentSyntax = "hh";
+let currentTabSize = 2;
+
 let htmlTextarea = document.querySelector("textarea[name=html]");
 let hsTextarea = document.querySelector("textarea[name=hs]");
+let syntaxHButton = document.querySelector("button[name=syntax][value=h]");
+let syntaxHHButton = document.querySelector("button[name=syntax][value=hh]");
+let tabSize2Button = document.querySelector("button[name=tabSize][value='2']");
+let tabSize4Button = document.querySelector("button[name=tabSize][value='4']");
 
 let inputEditor = codeMirror.fromTextArea(htmlTextarea, {
   mode: "htmlmixed",
@@ -35,18 +41,23 @@ let outputEditor = codeMirror.fromTextArea(hsTextarea, {
   //lineNumbers: true,
 });
 
-inputEditor.on("change", debounce(cm => {
-  console.log("Input changed!");
+let updateOutputEditor = function() {
   let inputValue = inputEditor.getValue();
   let outputValue;
-  let [outputHtml, usedTagNames] = convert2(inputValue);
+  let [outputHtml, usedTagNames] =  htmlToHs2({syntax: currentSyntax, tabSize: currentTabSize}, inputValue);
   if (usedTagNames.length) {
-    outputValue = outputHtml + "\n\n" + `// let {${join(", ", usedTagNames)}} = hh(h);\n\n`;
+    if (currentSyntax == "hh") {
+      outputValue = outputHtml + "\n\n" + `// let {${join(", ", usedTagNames)}} = hh(h);`;
+    } else {
+      outputValue = outputHtml;
+    }
   } else {
     outputValue = outputHtml;
   }
   outputEditor.setValue(outputValue);
-}, 500));
+};
+
+inputEditor.on("change", debounce(updateOutputEditor, 500));
 
 let initHtml = `
 <div class="menu">
@@ -59,3 +70,32 @@ let initHtml = `
 inputEditor.focus();
 inputEditor.setValue(initHtml.trim());
 
+syntaxHButton.addEventListener("click", function (event) {
+  currentSyntax = "h";
+  syntaxHHButton.classList.remove("active");
+  syntaxHButton.classList.add("active");
+  updateOutputEditor();
+});
+
+syntaxHHButton.addEventListener("click", function (event) {
+  currentSyntax = "hh";
+  syntaxHButton.classList.remove("active");
+  syntaxHHButton.classList.add("active");
+  updateOutputEditor();
+});
+
+tabSize2Button.addEventListener("click", function (event) {
+  console.log("tabSize2Button click");
+  currentTabSize = 2;
+  tabSize4Button.classList.remove("active");
+  tabSize2Button.classList.add("active");
+  updateOutputEditor();
+});
+
+tabSize4Button.addEventListener("click", function (event) {
+  console.log("tabSize4Button click");
+  currentTabSize = 4;
+  tabSize2Button.classList.remove("active");
+  tabSize4Button.classList.add("active");
+  updateOutputEditor();
+});
